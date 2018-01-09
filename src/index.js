d7a7c9c01d225f328f74b6468c8d339b2183669f
Peter Skopal <peter.skopal@hotmail.com>
2018-01-09 12:11:38 +0100

    feat(mountNode): Beeing able to modify id on div to be mounted on in production.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const mountNode = document.currentScript.getAttribute('mountNode') || 'savings-calculator'
+
+ReactDOM.render(<App />, document.getElementById(mountNode));
 registerServiceWorker();
