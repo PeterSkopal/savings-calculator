083d9792a4c1acfc3b56dd75443f01a4c6c5bc8f
Peter Skopal <peter.skopal@hotmail.com>
2018-01-09 15:54:07 +0100

    feat: standard sentences/queries.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const DefaultMessages = {
+  SavingQuestion: 'Do you want to know how much you can save in 10, 25 or 50 years?',
+  InitialSavings: 'What is your initial savings capital?',
+  IncomeQuery: 'What is your income?',
+  InterestQuery: 'How much yield are you expecting each year?',
+  MonthlySavings: 'How much will you invest each month?'
+}
+const Messages = {
+  SavingQuestion: document.currentScript.getAttribute('SavingQuestion') || DefaultMessages.SavingQuestion,
+  InitialSavings: document.currentScript.getAttribute('InitialSavings') || DefaultMessages.InitialSavings,
+  IncomeQuery: document.currentScript.getAttribute('IncomeQuery') || DefaultMessages.IncomeQuery,
+  InterestQuery: document.currentScript.getAttribute('InterestQuery') || DefaultMessages.InterestQuery,
+  MonthlySavings: document.currentScript.getAttribute('MonthlySavings') || DefaultMessages.MonthlySavings
+}
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
